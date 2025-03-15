b1fba720ba967a0408202a6341cfa677df3a9c47
ravali mukkavilli <ravalimukkavilli@gmail.com>
2025-03-14 21:03:07 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import About from "./components/About";
 import Skills from "./components/Skills";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import Navbar from "./components/navbar";
+import About from "./components/about";
 
 function App() {
   return (
